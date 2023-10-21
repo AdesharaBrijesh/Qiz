080ec18035750e261934b4077f1b857c294a8670
AdesharaBrijesh <adesharabrijesh8@gmail.com>
2023-10-22 01:04:56 +0530

    Designed MainActivity UI.

--- app/src/main/java/com/example/qiz/MainActivity.kt
+++ app/src/main/java/com/example/qiz/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.qiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.content.Intent
+import android.provider.SyncStateContract
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        //This call the parent constructor
         super.onCreate(savedInstanceState)
+        // This is used to align the xml view to this class
         setContentView(R.layout.activity_main)
+
     }
 }
