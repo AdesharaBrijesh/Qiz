11d0b2d15cd5e25315f1c20bf38db018fa6fb1c6
AdesharaBrijesh <adesharabrijesh8@gmail.com>
2023-10-22 16:24:56 +0530

    Designed activity_quiz_questions.xml file and made some changes in kotlin files.

--- app/src/main/java/com/example/qiz/MainActivity.kt
+++ app/src/main/java/com/example/qiz/MainActivity.kt
@@ -2,12 +2,11 @@ package com.example.qiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.content.Intent
-import android.provider.SyncStateContract
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.view.WindowCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +15,24 @@ class MainActivity : AppCompatActivity() {
         // This is used to align the xml view to this class
         setContentView(R.layout.activity_main)
 
+        // To display the activity in full screen and also make the status bar and notification bar transparent in themes.xml
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        val btn_start = findViewById<Button>(R.id.btn_start)
+        val et_name = findViewById<AppCompatEditText>(R.id.et_name)
+
+        btn_start.setOnClickListener {
+
+            // here the isBlank() function is used in the condition to check if the string is empty or contains only spaces.
+            if (et_name.text.toString().isBlank()) {
+                // if yes then don't proceed further and show a toast message.
+                Toast.makeText(this@MainActivity, "Please enter your name", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                // If the string is valid continue to the next activity.
+                Intent(this, QuizQuestionsActivity::class.java).apply { startActivity(this) }
+                finish()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/qiz/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/qiz/QuizQuestionsActivity.kt
@@ -2,10 +2,43 @@ package com.example.qiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 
 class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
+
+        val questionsList = Constants().getQuestions()
+        Log.e("Questions Size", "${questionsList.size}") // To print the no. of questions in logcat.
+        for (i in questionsList) {
+            Log.e("Questions", i.question) // To print all the questions in logcat.
+        }
+
+        val currentPosition = 1 // Default and the first question position
+        val question: Question? = questionsList[currentPosition - 1] // Getting the question from the list with the help of current position.
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        val tv_progress = findViewById<TextView>(R.id.tv_progress)
+        val tv_question = findViewById<TextView>(R.id.tv_question)
+        val iv_image = findViewById<ImageView>(R.id.iv_image)
+        val tv_option_one = findViewById<TextView>(R.id.tv_option_one)
+        val tv_option_two = findViewById<TextView>(R.id.tv_option_two)
+        val tv_option_three = findViewById<TextView>(R.id.tv_option_three)
+        val tv_option_four = findViewById<TextView>(R.id.tv_option_four)
+
+        progressBar.progress = currentPosition // Setting the current progress in the progressbar using the position of question
+        tv_progress.text = "$currentPosition" + "/" + progressBar.getMax() // Setting up the progress text
+
+        // Now set the current question and the options in the UI
+        tv_question.text = question!!.question
+        iv_image.setImageResource(question.image)
+        tv_option_one.text = question.optionOne
+        tv_option_two.text = question.optionTwo
+        tv_option_three.text = question.optionThree
+        tv_option_four.text = question.optionFour
+
     }
 }
